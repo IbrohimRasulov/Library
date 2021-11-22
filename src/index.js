2ecec9c45cec019bbc33735b757129f49184e435
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-11-22 16:54:45 +0500

    Try to build the project in another way

--- src/index.js
+++ src/index.js
@@ -5,47 +5,120 @@ const book_list = document.querySelector('.list-group');
 const title = document.querySelector('#book-title');
 const author = document.querySelector('#book-author');
 
-// const books = JSON.parse(localStorage.getItem('books')) || [];
-const books = [];
-
-
-const addToLocalStorage = (title, author) => {
-  books.push({
-    title,
-    author
-  });
-
-  localStorage.setItem('books', JSON.stringify(books));
-
-  return { title, author }; 
+function Book(title, author) {
+  this.title = title.value;
+  this.author = author.value;
 }
 
-const show_book = () => {
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
   const li = document.createElement('li');
-  const p = document.createElement('p');
-  const button = document.createElement('button');
+  
+  li.innerHTML = `
+  <p class="font-monospace d-inline">${title.value}" by ${author.value}</p>
+  <button class="btn btn-danger btn-sm float-end">Delete</button>
+  `;
 
   li.classList = 'list-group-item';
-  p.innerText = `"${title.value}" by ${author.value}`;
-  p.classList = 'font-monospace d-inline';
-  button.innerText = 'Delete';
-  button.classList = 'btn btn-danger btn-sm float-end';
 
-  li.appendChild(p);
-  li.appendChild(button);
   book_list.appendChild(li);
+}
+
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'btn-danger') {
+    target.parentElement.remove();
+  }
 }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const new_book = addToLocalStorage(
-    title.value,
-    author.value
-  );
+  const book = new Book(title, author);
 
-  show_book(new_book);
+  const ui = new UI();
+
+  ui.addBookToList(book);
 
   title.value = '';
   author.value = '';
+})
+
+book_list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const books = JSON.parse(localStorage.getItem('books')) || [];
+
+// const addToLocalStorage = (title, author) => {
+//   books.push({
+//     title,
+//     author
+//   });
+
+//   localStorage.setItem('Library', JSON.stringify(books));
+
+//   return { title, author }; 
+// }
+
+// const create_book = ({ title, author }) => {
+//   const li = document.createElement('li');
+//   const p = document.createElement('p');
+//   const button = document.createElement('button');
+
+//   li.classList = 'list-group-item';
+//   p.innerText = `"${title.value}" by ${author.value}`;
+//   p.classList = 'font-monospace d-inline';
+//   button.innerText = 'Delete';
+//   button.classList = 'btn btn-danger btn-sm float-end';
+
+//   li.appendChild(p);
+//   li.appendChild(button);
+//   book_list.appendChild(li);
+// }
+
+// books.forEach(create_book);
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   const new_book = addToLocalStorage(
+//     title.value,
+//     author.value
+//   );
+
+//   create_book(new_book);
+
+//   title.value = '';
+//   author.value = '';
+// });
