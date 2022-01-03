5fb8ebdba63560b922eee143f57ffab796629047
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-01-03 18:48:27 +0500

    Reset the project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './style.css';
 
+const form = document.querySelector('form');
+const list = document.querySelector('.list-group');
+const title = document.querySelector('#book-title');
+const author = document.querySelector('#book-author');
+
